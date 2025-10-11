f3c9a0f84744eea2b3290742cdb1b5fbf0d2bd5c
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2025-10-11 11:49:52 +0600

    working on login and registrationapi started

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import views, status
 
-# Create your views here.
+from .serializers import (
+    LoginSerializer,
+    RegistrationSerializer
+)
+
+class LoginView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(
+            {
+                "msg": "working",
+            },
+            status=status.HTTP_200_OK
+        )
+
+class RegistrationView(APIView):
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(
+            {
+                "msg": "working"
+            },
+            status=status.HTTP_200_OK
+        )
